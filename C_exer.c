4c5f9482bc01f86b9b83b73ccdf59b74e60d8945
Schwa_Xiping <xipingevan@foxmail.com>
2026-05-18 21:40:58 +0800

    Update C_exer.c

--- C_exer.c
+++ C_exer.c
@@ -227,3 +227,62 @@ int main(void){
     return 0;
 }
 
+#include<stdio.h>
+
+int main(void){
+
+  int DAY, MONTH, YEAR;
+
+  printf("Enter date (mm/dd/yy): ");
+  scanf("%d/%d/%d", &MONTH, &DAY, &YEAR);
+
+  printf("Dated this %d", DAY);
+
+  switch (DAY)
+  {
+  case 1: case 11: case 21: case 31: 
+    printf("st");
+    break;
+  case 2: case 12: case 22:
+    printf("nd");
+    break;
+  case 3: case 13: case 23:
+    printf("rd");
+  default: printf("th");
+    break;
+  }
+
+  printf(" of ");
+
+  switch (MONTH)
+  {
+  case 1: printf("January");
+    break;
+  case 2: printf("February");
+    break;
+  case 3: printf("March");
+    break;
+  case 4: printf("April");
+    break;
+  case 5: printf("May");
+    break;
+  case 6: printf("June");
+    break;
+  case 7: printf("July");
+    break;
+  case 8: printf("August");
+    break;
+  case 9: printf("September");
+    break;
+  case 10: printf("October");
+    break;
+  case 11: printf("November");
+    break;
+  case 12: printf("December");
+    break;
+  }
+
+  printf(", %d", YEAR);
+
+  return 0;
+}
